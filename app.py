c155bf20e31ba639c2c0c62affb897611190047b
Carl Bäckström <genzorg@gmail.com>
2018-10-26 21:22:29 +0200

    Part 3 - Forms and User Input - Added registration and login forms and registration html https://www.youtube.com/watch?v=UIJKdCIEXUQ

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, url_for
 
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+# noinspection SpellCheckingInspection
+# import secrets
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = 'bcaa436189daf75374ecebec4a652522'
 
 posts = [
     {
@@ -27,6 +33,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html", title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
