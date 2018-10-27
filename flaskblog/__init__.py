6de0c2f0cfd1efd9a905a4a21127d5d58b0624d9
Carl Bäckström <genzorg@gmail.com>
2018-10-27 15:51:17 +0200

    Part 6 - User Authentication - User is stored in DB with a hashed password using flask-bcrypt https://www.youtube.com/watch?v=CSHx6eCkmv0

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 # noinspection SpellCheckingInspection
@@ -9,6 +10,7 @@ app.config['SECRET_KEY'] = 'bcaa436189daf75374ecebec4a652522'
 # The three slashes means a relative path so the file will next to the script
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 # This import is here to avoid circular imports.  If they were at the top of the file routes.py
 # would try to import app before it was initialized.
