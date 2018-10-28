2587cb97d15ff92f5ec6e69056337a9d25b5d420
Carl Bäckström <genzorg@gmail.com>
2018-10-28 14:22:28 +0100

    Part 7 - User Account and Profile Picture - Added UpdateAcountForm to account route https://youtu.be/803Ei2Sq-Zs?t=766

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 
 
@@ -73,5 +73,6 @@ def logout():
 @app.route('/account')  # wrong order will not enable the login_required decorator
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = url_for('static', filename=f'profile_pictures/{current_user.image_file}')
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
