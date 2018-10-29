b90ed9a70fc439569395b7736698f37854a74c03
Carl Bäckström <genzorg@gmail.com>
2018-10-29 14:26:13 +0100

    Part 8 - Create, Update, and Delete Posts - Added view for viewing an existing post https://youtu.be/u0oDDZrDz9U?t=1485

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -106,3 +106,10 @@ def new_post():
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route("/post/<post_id>")
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
