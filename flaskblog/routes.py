2b312d7231077bf389666db359839198dc0fcc1b
Carl Bäckström <genzorg@gmail.com>
2018-10-29 14:35:21 +0100

    Part 8 - Create, Update, and Delete Posts - Removed the requirement to be logged in to edit a post https://youtu.be/u0oDDZrDz9U

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -109,7 +109,6 @@ def new_post():
 
 
 @app.route("/post/<post_id>")
-@login_required
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
