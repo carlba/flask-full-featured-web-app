43cb4b10bed754debfa2d651e4a471627cf89cac
Carl Bäckström <genzorg@gmail.com>
2018-10-29 22:43:32 +0100

    Part 9 - Pagination - Sort the post using order_by in the SQLAlchemy query https://youtu.be/PSWf2TjTGNY?t=1159

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -14,7 +14,7 @@ from flaskblog.models import User, Post
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(per_page=5, page=page)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template("home.html", posts=posts)
 
 
