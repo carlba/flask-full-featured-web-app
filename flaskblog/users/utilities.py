43885b96999ad74b4378e322dc24e6486ddce3fb
Carl Bäckström <genzorg@gmail.com>
2018-10-31 16:12:13 +0100

    Part 11 - Blueprints and Configuration - Moved app creation into create_app function https://youtu.be/Wfx4YBzg16s?t=2316

--- flaskblog/users/utilities.py
+++ flaskblog/users/utilities.py
@@ -2,10 +2,10 @@ import os
 import secrets
 
 from PIL import Image
-from flask import url_for
+from flask import url_for, current_app
 from flask_mail import Message
 
-from flaskblog import app, mail
+from flaskblog import mail
 
 
 def save_picture(form_picture_data):
@@ -13,7 +13,7 @@ def save_picture(form_picture_data):
     _, extension = os.path.splitext(form_picture_data.filename)
     new_picture_filename = f'{random_hex}{extension}'
     # TODO: Can saving the file be done without manual path handling?
-    picture_path = os.path.join(app.root_path, 'static/profile_pictures', new_picture_filename)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pictures', new_picture_filename)
 
     # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html
     output_size = (125, 125)
@@ -26,7 +26,7 @@ def save_picture(form_picture_data):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    message = Message('Password Reset Request', sender=app.config['MAIL_USERNAME'],
+    message = Message('Password Reset Request', sender=current_app.config['MAIL_USERNAME'],
                       recipients=[user.email])
     # The _external keyword argument makes sure that an absolute url is generated.
     message.body = f'''To reset your password, visit the following link:
